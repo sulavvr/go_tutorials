37b2da80967318b7435a49976d2846da6753d31e
Sulav Regmi <sulavvr@gmail.com>
2016-11-06 20:48:05 -0600

    Solve matching brackets; Failed one test case (#9) (wrong answer)

---
+++ hackerrank/matching_brackets/main.go
@@ -0,0 +1,77 @@
+package main
+
+/**
+ * Hackerrank
+ * Cracking the coding interview > Stacks: Balanced Brackets
+ */
+
+import (
+	"fmt"
+)
+
+var stack []string
+
+var brackets = map[string]string{
+	"(": ")",
+	"{": "}",
+	"[": "]",
+}
+
+func main() {
+
+	var count int
+	var strings []string
+
+	fmt.Scanf("%d", &count)
+
+	for i := 0; i < count; i++ {
+		var str string
+		fmt.Scanf("%s", &str)
+
+		strings = append(strings, str)
+	}
+
+	// {[()]}
+
+	// [ {, [, (,  ]
+	for i, _ := range strings {
+		for _, x := range strings[i] {
+			if _, ok := brackets[string(x)]; ok {
+				push(string(x))
+			} else {
+				if exists := checkIfPrev(string(x)); exists {
+					pop()
+				}
+			}
+		}
+		if len(stack) > 0 {
+			fmt.Println("NO")
+		} else {
+			fmt.Println("YES")
+		}
+		clearStack()
+	}
+
+}
+
+func checkIfPrev(val string) bool {
+	if len(stack) > 0 {
+		in_stack := stack[len(stack)-1] // (
+		if v, _ := brackets[in_stack]; v == val {
+			return true
+		}
+	}
+	return false
+}
+
+func push(val string) {
+	stack = append(stack, val)
+}
+
+func pop() {
+	stack = stack[:(len(stack) - 1)]
+}
+
+func clearStack() {
+	stack = []string{}
+}
